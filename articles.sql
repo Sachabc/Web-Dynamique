8ee7c90cf84bf858512248d2026ada0ce7cab460
Gab02082005 <149966853+Gab02082005@users.noreply.github.com>
2025-06-01 17:49:27 +0200

    Update articles.sql

--- articles.sql
+++ articles.sql
@@ -1,24 +1,6 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:8889
--- Généré le : mar. 27 mai 2025 à 14:59
--- Version du serveur : 8.0.40
--- Version de PHP : 8.3.14
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `agora_francia`
+ncia`
 --
 
 -- --------------------------------------------------------
